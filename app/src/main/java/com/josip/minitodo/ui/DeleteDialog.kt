56de2151fa7d2cfd799d2d62f9658a298a1669c4
josip-g <josip.g.github@gmail.com>
2025-07-28 22:52:11 +0200

    Change type of Delete dialog buttons

--- app/src/main/java/com/josip/minitodo/ui/DeleteDialog.kt
+++ app/src/main/java/com/josip/minitodo/ui/DeleteDialog.kt
@@ -1,8 +1,9 @@
 package com.josip.minitodo.ui
 
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 
 @Composable
@@ -19,12 +20,12 @@ fun DeleteDialog(
         title = { Text(title) },
         text = { Text(message) },
         confirmButton = {
-            TextButton(onClick = onConfirm) {
+            OutlinedButton(onClick = onConfirm) {
                 Text(confirmButtonText)
             }
         },
         dismissButton = {
-            TextButton(onClick = onDismiss) {
+            Button(onClick = onDismiss) {
                 Text(dismissButtonText)
             }
         }
